b5beebb85153cb7af10a5c16d0e1c98ff12b6d41
AvanNorth <avannorthy@users.noreply.github.com>
2020-04-07 18:47:58 +0300

    work

--- src/main/java/PriceCommand.java
+++ src/main/java/PriceCommand.java
@@ -7,32 +7,33 @@ import org.telegram.telegrambots.meta.bots.AbsSender;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public final class NaebCommand extends BotsCommand {
+public final class PriceCommand extends BotsCommand {
 
     private final ICommandRegistry mCommandRegistry;
 
-    public NaebCommand(ICommandRegistry commandRegistry) {
-        super("naeb", "sosi");
+    public PriceCommand(ICommandRegistry commandRegistry) {
+        super("price", "sosi");
         mCommandRegistry = commandRegistry;
     }
     @Override
     public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
         if (chat.getId() == 615213408){
-
             final String regex = "\\D+";
             final Pattern pattern = Pattern.compile(regex);
             final Matcher matcher = pattern.matcher(strings[0]);
             if (matcher.find()) {
                 char[] helloArray1 = strings[0].toCharArray();
-                char[] helloArray = new char[9];
-                for (int i=0;i<9;i++){
-                    helloArray[i] = helloArray1[i];
-                }
-                String helloString = new String(helloArray);
+                char[] idArray = new char[9];
+                char[] priceArray = new char[4];
+                System.arraycopy(helloArray1, 0, idArray, 0, 9);
+                System.arraycopy(helloArray1, 10, priceArray, 0, 4);
+                String helloString = new String(idArray);
+                String priceString = new String(priceArray);
                 System.out.println("Full match: " + matcher.group(0));
                 SendMessage message = new SendMessage();
                 message.setChatId(Long.parseLong(helloString,10));
-                message.setText(matcher.group(0));
+                message.setText(priceString);
+                System.out.println(priceString);
                 execute(absSender, message, user);
             }
         }
@@ -42,7 +43,6 @@ public final class NaebCommand extends BotsCommand {
             message.setChatId(chat.getId());
             execute(absSender,message,user);
         }
-
 
     }
 }
